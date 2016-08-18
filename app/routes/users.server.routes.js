b099e6de28df52022b89ebd45515453b7b2bff95
Roman Alshehri <aalshehri08@hotmail.com>
2016-08-17 22:04:04 -0700

    Branck

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var user = require('../controllers/users.server.controller');
+
+module.exports = function (app) {
+	app.route('/users')
+	   .get(user.list)
+	   .post(user.create);
+
+	app.route('/users/:userId')
+	 	.get(user.view)
+	 	.put(user.update)
+	 	.delete(user.delete);
+
+	app.param('userId', user.userById);
+}
